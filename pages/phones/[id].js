02e06119a9eb30d208c79c8208ff9b35678f6cef
Kevin Townson <ktown5422@gmail.com>
2021-02-25 13:25:21 -0600

    added phones detailed view once image is clicked

--- pages/phones/[id].js
+++ pages/phones/[id].js
@@ -1,9 +1,19 @@
+import Head from 'next/head'
 import phones from '../../phones'
+import '../index'
 
 export default function Phone(props) {
     return (
-      <div>
-        <p>{props.phone.title}</p>
+        <div>
+            <Head>
+            <title>{props.phone.title}</title>
+            </Head>
+            <img src={props.phone.picture} height='100px' width='100px' alt='Phone picture' />
+            <p>
+            {props.phone.type}
+            </p>
+            <p>{props.phone.title}</p>
+            <p>{props.phone.description}</p>
       </div>
     )
   }
@@ -11,7 +21,8 @@ export default function Phone(props) {
 
 
 export async function getServerSideProps({ query }) {
-    console.log(query)
+    const { id } = query
+
     return { 
         props: {
             phone: phones.filter((phone) => phone.id === parseInt(id))[0],
